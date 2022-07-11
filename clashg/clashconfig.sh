4480bc1432dd09b6c401616521e5eb3572f3fe53
zhudan <shidan.zhu@dmall.com>
2022-07-11 18:19:10 +0800

    fix

--- clashg/clashconfig.sh
+++ clashg/clashconfig.sh
@@ -47,25 +47,25 @@ rm_nat(){
 #  iptables -t nat -D PREROUTING -p tcp -m set --match-set $dnsmasq_gfw_ipset dst -j REDIRECT --to-port "$proxy_port"
 #  iptables -t nat -D PREROUTING -p tcp -m set --match-set $gfw_cidr_ipset dst -j REDIRECT --to-port "$proxy_port"
 	LOGGER 删除iptables开始 >> $LOG_FILE
-#	ipset_indexs=$(iptables -t nat -vnL PREROUTING --line-number  | sed 1,2d | sed -n "/${proxy_port}/=" | sort -n)
+#	ipset_indexs=$(iptables -t nat -vnL PREROUTING --line-number  | sed 1,2d | sed -n "/${proxy_port}/=" | sort -r)
 #  for ipset_index in $ipset_indexs; do
 #    iptables -t nat -D PREROUTING $ipset_index >/dev/null 2>&1
 #  done
 
   #tproxy模式
-  ip rule del fwmark 10 table 100
-  ip route del local 0.0.0.0/0 dev lo table 100
-	ipset_indexs=$(iptables -t mangle -vnL PREROUTING --line-number  | sed 1,2d | sed -n "/${tproxy_port}/=" | sort -n)
+  ip rule del fwmark 10 table 100 >/dev/null 2>&1
+  ip route del local 0.0.0.0/0 dev lo table 100 >/dev/null 2>&1
+	ipset_indexs=$(iptables -t mangle -vnL PREROUTING --line-number  | sed 1,2d | sed -n "/${tproxy_port}/=" | sort -r)
   for ipset_index in $ipset_indexs; do
     iptables -t mangle -D PREROUTING $ipset_index >/dev/null 2>&1
   done
 
 	# 清理mixedport端口
-	ipset_indexs=$(iptables -vnL INPUT --line-number | sed 1,2d | sed -n "/${mixedport}/=" | sort -n)
+	ipset_indexs=$(iptables -vnL INPUT --line-number | sed 1,2d | sed -n "/${mixedport}/=" | sort -r)
 	for ipset_index in $ipset_indexs; do
 		iptables -D INPUT $ipset_index >/dev/null 2>&1
 	done
-	ipset_indexs=$(ip6tables -vnL INPUT --line-number | sed 1,2d | sed -n "/${mixedport}/=" | sort -n)
+	ipset_indexs=$(ip6tables -vnL INPUT --line-number | sed 1,2d | sed -n "/${mixedport}/=" | sort -r)
 	for ipset_index in $ipset_indexs; do
 		ip6tables -D INPUT $ipset_index >/dev/null 2>&1
 	done
