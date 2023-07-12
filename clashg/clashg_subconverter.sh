5ae021dcf56b26d58c20eb0542a3387c269a2ced
zhudan <shidan.zhu>
2023-07-12 14:27:10 +0800

    自定义UA

--- clashg/clashg_subconverter.sh
+++ clashg/clashg_subconverter.sh
@@ -13,6 +13,27 @@ failed_warning_clash(){
   exit 1
 }
 
+generate_random_user_agent() {
+  local random_index
+  local max_index=9
+  local random_bytes
+  random_bytes=$(tr -dc '0-9' < /dev/urandom | head -c2 | sed 's/^0*//')
+  random_index=$((random_bytes % (max_index+1)))
+  local user_agent
+  case $random_index in
+    0) user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" ;;
+    1) user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36" ;;
+    2) user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0" ;;
+    3) user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/92.0.902.55 Safari/537.36" ;;
+    4) user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 YaBrowser/21.6.1.289 Yowser/2.5 Safari/537.36" ;;
+    5) user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; Trident/7.0; rv:11.0) like Gecko" ;;
+    6) user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36" ;;
+    7) user_agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" ;;
+    8) user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36" ;;
+    9) user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36" ;;
+  esac
+  echo "$user_agent"
+}
 start_online_update_hnd(){
   links="http://127.0.0.1:25500/sub?${subconverter_args}"
   LOGGER "subconverter进程：$(pidof subconverter)" >> $LOG_FILE
@@ -22,7 +43,7 @@ start_online_update_hnd(){
 
   #wget下载文件
   #wget --no-check-certificate -t3 -T30 -4 -O /tmp/upload/$upname "$links"
-  UA='Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
+  UA=$(generate_random_user_agent)
   LOGGER "使用常规网络下载..." >> $LOG_FILE
   curl -4sSk --user-agent "$UA" --connect-timeout 30 "$links" > $clash_sub_file_tmp
   LOGGER "配置文件下载完成 ${clash_sub_file_tmp}" >> $LOG_FILE
@@ -32,7 +53,8 @@ start_online_update_hnd(){
     if [ -n "$(cat $clash_sub_file_tmp | grep proxies:)" ] && [ -z "$(cat $clash_sub_file_tmp | grep proxy-groups:\ ~)" ]; then
       LOGGER "使用curl下载成功，但是内容不包含节点或者不包含proxy-groups，尝试更换wget进行下载: $links"	>> $LOG_FILE
       rm -rf $clash_sub_file_tmp
-      wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" --no-check-certificate -t3 -T30 -4 -O $clash_sub_file_tmp "$links"
+      UA=$(generate_random_user_agent)
+      wget --user-agent="$UA" --no-check-certificate -t3 -T30 -4 -O $clash_sub_file_tmp "$links"
     fi
     LOGGER "检查文件完整性" >> $LOG_FILE
     if [ -z "$(cat $clash_sub_file_tmp | grep proxies:)" ]; then
